61b2e0c99cd3dea38e9479c2aef664f864e80f52
Tobias Kadenbach <tobias.kadenbach@spork.codes>
2022-01-13 18:06:34 +0100

    Add first version of exporter

---
+++ exporter.py
@@ -0,0 +1,74 @@
+from drugs_for_disease_dict import DrugsForDiseaseDict
+
+
+class Exporter:
+    dicts = {}
+
+    def add_to_export(self, name, dict):
+        self.dicts[name] = dict
+
+    def export(self):
+        nodes = []
+        links = []
+        pre_nodes = {}
+        pre_links = []
+        for key in self.dicts:
+            ## export nodes
+            disease_node = Node(key, 0, key)
+            pre_nodes[key] = disease_node
+            for med in self.dicts[key]:
+                if med not in pre_nodes:
+                    pre_nodes[med] = Node(med, 1, med)
+            ## end export nodes
+            ## export links
+            for med in self.dicts[key]:
+                pre_links.append(Link(key, med, self.dicts[key][med]))
+            ## end export links
+        
+        for node in pre_nodes:
+            nodes.append(pre_nodes[node].to_string())
+        for link in pre_links:
+            links.append(link.to_string())
+
+        with open("export.txt", mode="w") as file:
+           file.write("var nodes = [")
+           for write_node in nodes:
+               file.write(write_node)
+           file.write("]")
+           file.write("var links = [")
+           for write_link in links:
+               file.write(write_link)
+           file.write("]")
+
+
+            
+
+
+
+
+
+class Node:
+    id = ""
+    group = 1
+    label = ""
+    def __init__(self, node_id, node_group, node_label):
+        self.id = node_id
+        self.group = node_group
+        self.label = node_label
+
+    def to_string(self):
+        pre_string = "id: '{}' , group: {} , label: '{}'".format(self.id, self.group, self.label)
+        return "{" + pre_string + "}"
+
+class Link:
+    target = ""
+    source = ""
+    strength  = 0
+    def __init__(self, node_target, node_source, node_strength):
+        self.target = node_target
+        self.source = node_source
+        self.strength = node_strength
+
+    def to_string(self):
+        pre_string = "target: '{}' , source: {} , strength: '{}'".format(self.target, self.source, self.strength)
+        return "{" + pre_string + "}"
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from drug_finder import DrugFinder
+from exporter import Exporter
 from pubmed_impl import PubmedImpl
 from spacy_impl import SpacyRecognizer
 from drugs_for_disease_dict import DrugsForDiseaseDict
@@ -6,29 +7,36 @@ from drugs_for_disease_dict import DrugsForDiseaseDict
 
 pubmed = PubmedImpl()
 
-diseaseQuery = "hepatitis"  # will only work if the string is a single word because of tokenization
+disease_list_string = input("Enter a list of diseases to query for seperated by ', '")
 
-# drugDict contains the name of the disease (.disease_name), the co-occurrence dictionary (.drug_dict)
-# and the odds ratio dictionary (.odds_ratio_dict)
-drugDict = DrugsForDiseaseDict(diseaseQuery)
+disease_list = disease_list_string.split(", ")
 
-maxPapers = 200  # limit the number of papers retrieved
-myQuery = diseaseQuery + "[tiab]"  # query in title and abstract
-records = pubmed.getPapers(myQuery, maxPapers, 'xxx.xxx@mailbox.tu-dresden.de')
+exporter = Exporter()
 
-# Concatenate Abstracts to one long string
-text: str = ''
-for r in records:
-    if 'AB' in r:
-        text += (r['AB'])
+for disease in disease_list:
+    diseaseQuery = disease  # will only work if the string is a single word because of tokenization
 
-# Named entity recognition by spacy
-# sp = SpacyRecognizer()
-# print(sp.qualify_text(text=text))
+    # drugDict contains the name of the disease (.disease_name), the co-occurrence dictionary (.drug_dict)
+    # and the odds ratio dictionary (.odds_ratio_dict)
+    drugDict = DrugsForDiseaseDict(diseaseQuery)
 
-# Fill drugDict with co-occurrences in text
-drug_finder = DrugFinder()
-drug_finder.qualify_text(text=text, drug_dict=drugDict)
+    maxPapers = 200  # limit the number of papers retrieved
+    myQuery = diseaseQuery + "[tiab]"  # query in title and abstract
+    records = pubmed.getPapers(myQuery, maxPapers, 'xxx.xxx@mailbox.tu-dresden.de')
 
-print(drugDict.drug_dict)
-print(drugDict.odds_ratio_dict)
+    # Concatenate Abstracts to one long string
+    text: str = ''
+    for r in records:
+        if 'AB' in r:
+            text += (r['AB'])
+
+    # Named entity recognition by spacy
+    # sp = SpacyRecognizer()
+    # print(sp.qualify_text(text=text))
+
+    # Fill drugDict with co-occurrences in text
+    drug_finder = DrugFinder()
+    drug_finder.qualify_text(text=text, drug_dict=drugDict)
+    exporter.add_to_export(disease, drugDict.odds_ratio_dict)
+
+exporter.export()
