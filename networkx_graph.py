4990d7cfd559cd3444b0e6dc26a6662e2404acf8
heavenduck <arndtwagner@mailbox.org>
2022-01-14 18:23:00 +0100

    anpassen der Ausgabe des Graphen

--- networkx_graph.py
+++ networkx_graph.py
@@ -2,12 +2,12 @@ import networkx as nx
 from fa2 import ForceAtlas2
 import matplotlib.pyplot as plt
 
-def graph(self,  dict ):
+def graph( data ):
     G = nx.Graph()
 
-    for disease in dict.keys():
-        for drug in dict.get(disease).keys():
-            G.add_edge(disease, drug, dict.get(disease).get(drug))
+    for disease in data:
+        for drug in data[disease]:
+            G.add_edge(disease , drug , weight=data[disease][drug])
 
     return G
 
@@ -34,7 +34,13 @@ def forceAtlas2Impl(G):
         verbose=True)
 
     positions = forceatlas2.forceatlas2_networkx_layout(G, pos=None, iterations=2000)
-    nx.draw_networkx_nodes(G, positions, node_size=20, with_labels=False, node_color="blue", alpha=0.4)
-    nx.draw_networkx_edges(G, positions, edge_color="green", alpha=0.05)
+    nx.draw_networkx_nodes(G, positions, node_size=20, node_color="black", alpha=0.4)
+    nx.draw_networkx_edges(G, positions, edge_color="darkblue", alpha=0.1)
     plt.axis('off')
     plt.show()
+
+
+if __name__ == "__main__":
+    dic = {'influenza': {'gallate': 0.14285714285714285, 'antibiotic': 1.0, 'antibacterials': 0.14285714285714285, 'antibiotics': 0.2857142857142857, 'RTIs': 0.14285714285714285, 'Lachnospiraceae': 0.14285714285714285, 'Ruminococcaceae': 0.14285714285714285, 'Ruminococcus': 0.14285714285714285, 'Capsaicin': 0.14285714285714285, 'oseltamivir': 0.5714285714285714, 'Tamiflu': 0.14285714285714285, 'zanamivir': 0.14285714285714285, 'January': 0.2857142857142857, 'diuretics': 0.14285714285714285, 'vasodilators': 0.14285714285714285, 'potato dextrose agar (PDA)': 0.14285714285714285, 'subglobose': 0.14285714285714285, 'ITS4': 0.14285714285714285, 'conidial': 0.14285714285714285, 'LSL3f2': 0.14285714285714285, 'hexylthiol': 0.14285714285714285, 'malachite green isothiocyanate (MGITC)': 0.14285714285714285, 'November': 0.14285714285714285, 'sialic acid receptor analogues': 0.14285714285714285, 'vRNPs': 0.42857142857142855, 'IAV antivirals targeting': 0.14285714285714285, 'September': 0.14285714285714285, 'Cyrillicnubis': 0.14285714285714285, 'diphenyltetrazolium bromide (MTT)': 0.14285714285714285, 'intratumoral': 0.14285714285714285, 'PD-1 inhibitors': 0.14285714285714285, 'October': 0.14285714285714285, 'Amparo': 0.14285714285714285, 'Pesquisa': 0.14285714285714285, 'December': 0.14285714285714285, 'February': 0.42857142857142855}}
+
+    forceAtlas2Impl(graph(dic))
