d1f85d46cd652f693563510a75ac581500181a6a
heavenduck <arndtwagner@mailbox.org>
2022-01-14 16:48:23 +0100

    funktion um die Daten im Dict zu speichern + Normalisierung des dicts

--- main_med.py
+++ main_med.py
@@ -1,14 +1,48 @@
 from pubmed_impl import PubmedImpl
-
 import spacy
 
+class DictCreator():
+
+  def __init__ (self):
+    self.dictStorage = {}
+
+  def getDict(self):
+    return self.dictStorage
+  
+  def getDictNormalized(self , threshhold=0):
+
+    dictStorageNorm = self.dictStorage
+
+    for key in dictStorageNorm:
+      max_apperance = max(dictStorageNorm[key].values())
+      dictStorageNorm[key] = {key:(val/max_apperance) for key, val in dictStorageNorm[key].items() if val > threshhold}   
+    
+    return dictStorageNorm
+
+
+  def addNewDisease(self , disease , doc_data):
+    
+    self.dictStorage[disease] = {}
+
+    for entity in doc_data.ents:
+      if entity.label_ == 'DRUG':
+        if entity.text in self.dictStorage[disease]:
+          self.dictStorage[disease][entity.text] += 1
+        else:
+          self.dictStorage[disease].update({ entity.text : 1}) 
+
+
+
+
 pubmed = PubmedImpl()
+drug_dict = DictCreator()
+
 med7 = spacy.load("en_core_med7_lg")
 
 diseaseQuery = 'influenza'
 
 
-maxPapers = 10  # limit the number of papers retrieved
+maxPapers = 50  # limit the number of papers retrieved
 myQuery = diseaseQuery + "[tiab]"  # query in title and abstract
 records = pubmed.getPapers(myQuery, maxPapers, 'xxx.xxx@mailbox.tu-dresden.de')
 
@@ -30,7 +64,8 @@ doc = med7(text)
 
 # spacy.displacy.serve(doc, style='ent', options=options)
 
+drug_dict.addNewDisease(diseaseQuery , doc)
+print(drug_dict.getDictNormalized())
+# print([(ent.text, ent.label_) for ent in doc.ents ])
 
-print(doc.ents)
 
-print([(ent.text, ent.label_) for ent in doc.ents ])
