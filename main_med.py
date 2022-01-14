ea0eb3ee51934a6f744ba07415b57d95aaa3f409
heavenduck <arndtwagner@mailbox.org>
2022-01-14 18:23:29 +0100

    Unterstützung mehrer Krankheiten -> Bug mit threshhold > 0

--- main_med.py
+++ main_med.py
@@ -1,5 +1,7 @@
 from pubmed_impl import PubmedImpl
 import spacy
+from networkx_graph import *
+
 
 class DictCreator():
 
@@ -9,16 +11,6 @@ class DictCreator():
   def getDict(self):
     return self.dictStorage
   
-  def getDictNormalized(self , threshhold=0):
-
-    dictStorageNorm = self.dictStorage
-
-    for key in dictStorageNorm:
-      max_apperance = max(dictStorageNorm[key].values())
-      dictStorageNorm[key] = {key:(val/max_apperance) for key, val in dictStorageNorm[key].items() if val > threshhold}   
-    
-    return dictStorageNorm
-
 
   def addNewDisease(self , disease , doc_data):
     
@@ -39,33 +31,38 @@ drug_dict = DictCreator()
 
 med7 = spacy.load("en_core_med7_lg")
 
-diseaseQuery = 'influenza'
+disease_list_string = 'hepatitis, cancer, diabetes'
+disease_list = disease_list_string.split(", ")
 
+for disease in disease_list:
+  
+  diseaseQuery = disease  # will only work if the string is a single word because of tokenization
 
-maxPapers = 50  # limit the number of papers retrieved
-myQuery = diseaseQuery + "[tiab]"  # query in title and abstract
-records = pubmed.getPapers(myQuery, maxPapers, 'xxx.xxx@mailbox.tu-dresden.de')
+  maxPapers = 500  # limit the number of papers retrieved
+  myQuery = diseaseQuery + "[tiab]"  # query in title and abstract
+  records = pubmed.getPapers(myQuery, maxPapers, 'xxx.xxx@mailbox.tu-dresden.de')
 
-# Concatenate Abstracts to one long string
-text: str = ''
-for r in records:
-  if 'AB' in r:
-    text += (r['AB'])
+  # Concatenate Abstracts to one long string
+  text: str = ''
+  for r in records:
+    if 'AB' in r:
+      text += (r['AB'])
 
-# create distinct colours for labels
-col_dict = {}
-seven_colours = ['#e6194B', '#3cb44b', '#ffe119', '#ffd8b1', '#f58231', '#f032e6', '#42d4f4']
-for label, colour in zip(med7.pipe_labels['ner'], seven_colours):
+  # create distinct colours for labels
+  col_dict = {}
+  seven_colours = ['#e6194B', '#3cb44b', '#ffe119', '#ffd8b1', '#f58231', '#f032e6', '#42d4f4']
+  for label, colour in zip(med7.pipe_labels['ner'], seven_colours):
     col_dict[label] = colour
 
-options = {'ents': med7.pipe_labels['ner'], 'colors':col_dict}
+  options = {'ents': med7.pipe_labels['ner'], 'colors':col_dict}
 
-doc = med7(text)
+  doc = med7(text)
 
-# spacy.displacy.serve(doc, style='ent', options=options)
+  # spacy.displacy.serve(doc, style='ent', options=options)
 
-drug_dict.addNewDisease(diseaseQuery , doc)
-print(drug_dict.getDictNormalized())
-# print([(ent.text, ent.label_) for ent in doc.ents ])
+  drug_dict.addNewDisease(diseaseQuery , doc)
+  print(drug_dict.getDictNormalized(3))
 
-
+datadict = drug_dict.getDictNormalized(1)
+g = graph(datadict)
+forceAtlas2Impl(g)
